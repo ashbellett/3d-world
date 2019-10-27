97e0822e44f9f6e425ff1a011ed0549d37b39221
Ash Bellett <ash.bellett.ab@gmail.com>
2019-10-27 22:43:45 +1100

    Fixed incorrect commit

--- server.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const app = express();
-const port = 3000;
-
-app.use(express.static('./3d-world'));
-app.get('/', (req, res) => res.send('On.'));
-app.listen(port, () => console.log(`Listening on port ${port}\n`));
